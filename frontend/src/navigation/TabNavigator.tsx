d8e4b403ac5763c2dc53e94b91c8ad2e6d88bb5b
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-04-16 20:32:24 +0200

    Grundgerüst vom Code erstellt und ein Prottyp für eine Navigationsleiste unten

---
+++ frontend/src/navigation/TabNavigator.tsx
@@ -0,0 +1,41 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MapScreen from "../screens/MapScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import CamScreen from "../screens/CamScreen";
+import Feather from "@expo/vector-icons/Feather";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="map" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="menu" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Camara"
+        component={CamScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="camera" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
---
+++ frontend/src/screens/ProfileScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const ProfileScreen = () => {
+  return (
+    <View>
+      <Text>Hier kann das Profil hinkommen</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
