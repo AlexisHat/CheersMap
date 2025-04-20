c10664bc56e534197dba2fefcd05b15e56084b6c
Florian Allofs <florian.allofs@gmx-topmail.de>
2025-04-20 20:56:13 +0200

    Karte und Camera Screen eingebunden

--- frontend/src/screens/CamScreen.tsx
+++ frontend/src/screens/CamScreen.tsx
@@ -1,12 +1,159 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  CameraMode,
+  CameraType,
+  CameraView,
+  useCameraPermissions,
+} from "expo-camera";
+import { useRef, useState } from "react";
+import { Image } from "expo-image";
+import * as Location from 'expo-location';
+import { Button, Pressable, Text, View, StyleSheet } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 export default function CamScreen() {
+  
+    
+  const [permission, requestPermission] = useCameraPermissions();
+  const ref = useRef<CameraView>(null);
+  const [uri, setUri] = useState<string | null>(null);
+  const [mode, setMode] = useState<CameraMode>("picture");
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [recording, setRecording] = useState(false);
+
+  if (!permission) {
+    return null;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to use the camera
+        </Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
+//Exif Daten=true
+  const takePicture = async () => {
+    const photo = await ref.current?.takePictureAsync({ base64: true, exif:true});
+    console.log(photo.base64); // Bilddaten als String
+    setUri(photo?.uri);
+
+    //Standort abfragen
+    const { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted') {
+    console.log('Standortzugriff verweigert');
+    return;
+  }
+  //Standort holen
+  const location = await Location.getCurrentPositionAsync({});
+
+  const data = {
+    imageUri: photo?.uri,
+    location: {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    },};
+
+    console.log("Foto + Standort:", data);
+    
+  
+
+  
+  };
+
+  const recordVideo = async () => {
+    if (recording) {
+      setRecording(false);
+      ref.current?.stopRecording();
+      return;
+    }
+    setRecording(true);
+    const video = await ref.current?.recordAsync();
+    console.log({ video });
+  };
+
+  const toggleMode = () => {
+    setMode((prev) => (prev === "picture" ? "video" : "picture"));
+  };
+  
+
+
+  const toggleFacing = () => {
+    setFacing((prev) => (prev === "back" ? "front" : "back"));
+  };
+
+  const renderPicture = () => {
+    return (
+      <View>
+        <Image
+          source={{ uri }}
+          contentFit="contain"
+          style={{ width: 300, aspectRatio: 1 }}
+        />
+        <Button onPress={() => setUri(null)} title="Take another picture" />
+      </View>
+    );
+  };
+
+  const renderCamera = () => {
+    return (
+      <CameraView
+        style={styles.camera}
+        ref={ref}
+        mode={mode}
+        facing={facing}
+        mute={false}
+        responsiveOrientationWhenOrientationLocked
+      >
+        <View style={styles.shutterContainer}>
+          <Pressable onPress={toggleMode}>
+            {mode === "picture" ? (
+              <AntDesign name="picture" size={32} color="white" />
+            ) : (
+              <Feather name="video" size={32} color="white" />
+            )}
+          </Pressable>
+          <Pressable onPress={mode === "picture" ? takePicture : recordVideo}>
+            {({ pressed }) => (
+              <View
+                style={[
+                  styles.shutterBtn,
+                  {
+                    opacity: pressed ? 0.5 : 1,
+                  },
+                ]}
+              >
+                <View
+                  style={[
+                    styles.shutterBtnInner,
+                    {
+                      backgroundColor: mode === "picture" ? "white" : "red",
+                    },
+                  ]}
+                />
+              </View>
+            )}
+          </Pressable>
+          <Pressable onPress={toggleFacing}>
+            <FontAwesome6 name="rotate-left" size={32} color="white" />
+          </Pressable>
+        </View>
+      </CameraView>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Dies ist der CamScreen!</Text>
+      {uri ? renderPicture() : renderCamera()}
     </View>
   );
+    
+  
 }
 
 const styles = StyleSheet.create({
