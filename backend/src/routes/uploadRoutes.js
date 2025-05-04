6896fcb801703e793034954792dc45fea668865b
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-05-04 17:08:00 +0200

    middleware an die upload Route gehangen

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/uploadController");
+const { authenticateToken } = require("../middlewares/authmiddleware");
 
-router.post("/create-post", controller.createPost);
+router.post("/create-post", authenticateToken, controller.createPost);
 
 module.exports = router;
