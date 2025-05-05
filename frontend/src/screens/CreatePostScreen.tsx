797f15b06fd00b82c9867ed338c317ae4eae9e6e
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-05-05 11:39:31 +0200

    User wird weitergeleitet wenn er einen Ort auswählt

---
+++ frontend/src/screens/CreatePostScreen.tsx
@@ -0,0 +1,9 @@
+import { View, ActivityIndicator } from "react-native";
+
+export const CreatePostScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ActivityIndicator size="small" />
+    </View>
+  );
+};
