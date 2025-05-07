adb20f42a931d13ffc8ef3f53e81e9c6a50b5367
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-05-07 23:53:43 +0200

    bug fix typo in den req params

--- frontend/src/services/locationService.ts
+++ frontend/src/services/locationService.ts
@@ -1,8 +1,8 @@
 import axios from "../api/api";
 
-export const fetchNearbyLocations = async (latitude: number, longitude: number) => {
+export const fetchNearbyLocations = async (latitude: number, longitude: number, max: number) => {
   const response = await axios.get("/locations/nearby", {
-    params: { lat: latitude, lon: longitude },
+    params: { lat: latitude, long: longitude, maxResults: max},
   });
   return response.data;
 };
