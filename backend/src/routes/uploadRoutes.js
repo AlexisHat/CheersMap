d3c60dc6054e8e51fe5b54d78b967187ba32bc64
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-05-13 12:03:02 +0200

    upload routes angepasst um die bilder richtig aus dem request zu holen

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 
 const controller = require("../controllers/uploadController");
 const { authenticateToken } = require("../middlewares/authmiddleware");
 
-router.post("/create-post", authenticateToken, controller.createPost);
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post(
+  "/create-post",
+  authenticateToken,
+  upload.fields([
+    { name: "frontImage", maxCount: 1 },
+    { name: "backImage", maxCount: 1 },
+  ]),
+  controller.createPost
+);
 
 module.exports = router;
