91a7ae67801d39bcaef264f7d7191762831f44f2
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-05-14 18:48:03 +0200

    createPost methode im service

--- backend/src/services/uploadService.js
+++ backend/src/services/uploadService.js
@@ -1,4 +1,7 @@
 const AWS = require("aws-sdk");
+const locationApiCache = require("./cache");
+const Location = require("../models/Location");
+const Post = require("../models/Post");
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -23,4 +26,48 @@ exports.uploadToS3 = async (file, key) => {
 
   await s3.upload(params).promise();
   return key;
+};
+
+exports.createPost = async (
+  locationId,
+  comment,
+  frontImageKey,
+  backImageKey
+) => {
+  if (!locationId || !comment || !frontImageKey || !backImageKey) {
+    throw new Error("Einer oder mehrere Parameter fehlen.");
+  }
+
+  let locationDoc = await Location.findOne({ placeId: locationId });
+
+  if (!locationDoc) {
+    const cachedPlace = locationApiCache.get(locationId);
+
+    if (!cachedPlace) {
+      throw new Error(
+        "Location nicht in Datenbank oder Cache gefunden. Bitte erst abrufen."
+      );
+    }
+
+    locationDoc = await Location.create({
+      name: cachedPlace.displayName?.text ?? "Unbenannt",
+      formattedAddress: cachedPlace.formattedAddress,
+      placeId: cachedPlace.id,
+      googleMapsUri: cachedPlace.googleMapsUri,
+      primaryType: cachedPlace.primaryType,
+      coordinates: {
+        lat: cachedPlace.location.latitude,
+        lng: cachedPlace.location.longitude,
+      },
+    });
+  }
+
+  const post = await Post.create({
+    location: locationDoc._id,
+    comment,
+    frontImageKey,
+    backImageKey,
+  });
+
+  return post;
 };
