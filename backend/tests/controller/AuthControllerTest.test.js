99d2356c4c27803c96c66416f96bbd5ebdc0710f
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2025-05-25 23:42:03 +0200

    test läuft jetzt durch und es gibt eine jest config

--- backend/tests/controller/AuthControllerTest.test.js
+++ backend/tests/controller/AuthControllerTest.test.js
@@ -1,15 +1,18 @@
 const request = require("supertest");
 const express = require("express");
-const authController = require("../src/controllers/authController");
+const authController = require("../../src/controllers/authController");
 
 const app = express();
 app.use(express.json());
 app.post("/login", authController.login);
 
-jest.mock("../src/services/AuthService", () => ({
+jest.mock("../../src/services/AuthService", () => ({
   loginUser: jest
     .fn()
     .mockResolvedValue({ accessToken: "ok", refreshToken: "ok" }),
+  registerUser: jest.fn(),
+  handleRefreshToken: jest.fn(),
+  logoutUser: jest.fn(),
 }));
 
 test("POST /login antwortet mit 200", async () => {
